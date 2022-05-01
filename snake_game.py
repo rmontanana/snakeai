b95207452f066f72e06a70d645f8ebcb34016a0e
Ricardo Montañana <rmontanana@gmail.com>
2022-05-02 01:05:28 +0200

    Begin agent

--- snake_game.py
+++ snake_game.py
@@ -83,7 +83,7 @@ class SnakeGame:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100 * len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -102,7 +102,7 @@ class SnakeGame:
         # 6. return game over and score
         return game_over, self.score
 
-    def _is_collision(self, point=None):
+    def is_collision(self, point=None):
         if point is None:
             point = self.head
         # hits boundary
